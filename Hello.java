c01ae3d9260ba3883d0ee0ec04f5635af154f54d
RodrigoConinck <rodrigo.coninck@hotmail.com>
2020-08-19 21:59:54 -0300

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta é uma saudação: ");
+		System.out.println("Hello, World! \n");
+		System.out.printf("Idade: %d\n", 20);
+	}
+}
